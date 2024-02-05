6e75951ab4f75898cd2913dcae351291ea11b442
MartinPSDev <martinperezsotelo.dev@gmail.com>
2024-02-04 22:51:46 -0300

    commit 2/4/2024

--- main.py
+++ main.py
@@ -1,33 +1,40 @@
-
-
 cantidad = int(input("Ingrese la cantidad de mazos: "))
 jugadores = int(input("Ingrese la cantidad de jugadores sin contar el crupier: "))
-primera_carta_jugador = tuple(input("Ingrese una de sus cartas en el siguiente formato [numero/letra, Palo]: ").split(', '))
-segunda_carta_jugador = tuple(input("Ingrese su otra carta en el siguiente formato [numero/letra, Palo]: ").split(', '))
-primera_carta_crupier = tuple(input("Ingrese una de las cartas del crupier en el siguiente formato [numero/letra, Palo]: ").split(', '))
-segunda_carta_crupier = tuple(input("Ingrese su otra carta en el siguiente formato [numero/letra, Palo]: ").split(', '))
 
+# Cartas de los jugadores
+cartas_jugadores = {}
 
+for i in range(1, jugadores + 1):
+    primera_carta_jugador = tuple(input(f"Ingrese la primera carta del jugador {i} en el siguiente formato [numero/letra, Palo]: ").split(', '))
+    segunda_carta_jugador = tuple(input(f"Ingrese la segunda carta del jugador {i} en el siguiente formato [numero/letra, Palo]: ").split(', '))
+    cartas_jugadores[f"jugador_{i}"] = (primera_carta_jugador, segunda_carta_jugador)
+
+# Cartas del crupier
+primera_carta_crupier = tuple(input("Ingrese la primera carta del crupier en el siguiente formato [numero/letra, Palo]: ").split(', '))
+segunda_carta_crupier = tuple(input("Ingrese la segunda carta del crupier en el siguiente formato [numero/letra, Palo]: ").split(', '))
 
 def crear_barajas(cantidad):
     palos = ['Corazones', 'Diamantes', 'Treboles', 'Picas']
     valores = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
     baraja = [(valor, palo) for palo in palos for valor in valores]
-    barajas_multiples = [((valor, palo), i+1) for i in range(cantidad) for valor, palo in baraja ]
+    barajas_multiples = [((valor, palo), i + 1) for i in range(cantidad) for valor, palo in baraja]
 
     return barajas_multiples
 
-nueva_baraja= crear_barajas(cantidad)
+nueva_baraja = crear_barajas(cantidad)
 
-#print (nueva_baraja)
-print(f'jugadores: {jugadores}\n\n sus cartas: {primera_carta_jugador}\t{segunda_carta_jugador}\n\n cartas del crupier:{primera_carta_crupier}\t{segunda_carta_crupier}\n\n')
- 
+# Mostrar cartas de los jugadores
+print("\nCartas de los jugadores:")
+for jugador, cartas in cartas_jugadores.items():
+    print(f'{jugador}: {cartas[0]}, {cartas[1]}')
 
-#Buscar las cartas para sacarlas del mazo
+# Mostrar cartas del crupier
+print(f'\nCartas del crupier: {primera_carta_crupier}, {segunda_carta_crupier}\n\n')
 
+# Buscar las cartas para sacarlas del mazo
 def buscar_cartas():
     global nueva_baraja
-    cartas_a_buscar = [primera_carta_jugador, segunda_carta_jugador, primera_carta_crupier, segunda_carta_crupier]
+    cartas_a_buscar = [carta for cartas in cartas_jugadores.values() for carta in cartas] + [primera_carta_crupier, segunda_carta_crupier]
     cartas_encontradas = []
 
     for carta in cartas_a_buscar:
@@ -35,11 +42,8 @@ def buscar_cartas():
             if carta_baraja[0] == carta:
                 cartas_encontradas.append(carta_baraja)
                 nueva_baraja.remove(carta_baraja)
-            
+
     return cartas_encontradas
 
-print(f'Cartas encontradas: {buscar_cartas()}\n\n')  
+print(f'Cartas encontradas: {buscar_cartas()}\n\n')
 print(f'Baraja Actual: {nueva_baraja} \n\n')
-
-      
-
